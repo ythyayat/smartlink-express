4b2f7ed0cc24159a196f842a2913264389c5cfd6
ythYayat <ythyayathidayat2@gmail.com>
2025-08-20 20:29:51 +0700

    fix: all user getting rate limited if someone got limited

--- middleware/rateLimitMiddleware.js
+++ middleware/rateLimitMiddleware.js
@@ -1,19 +1,25 @@
-import rateLimit from 'express-rate-limit';
+import rateLimit, { ipKeyGenerator } from 'express-rate-limit';
 
 export const createLinkLimiter = rateLimit({
   windowMs: 1 * 60 * 1000,
   max: 30,
-  message: 'Too many requests, please try again later.'
+  message: 'Too many requests, please try again later.',
+  trustProxy: true,
+  keyGenerator: ipKeyGenerator
 });
 
 export const redirectLinkLimiter = rateLimit({
   windowMs: 1 * 60 * 1000,
   max: 100,
-  message: 'Too many requests, please try again later.'
+  message: 'Too many requests, please try again later.',
+  trustProxy: true,
+  keyGenerator: ipKeyGenerator
 });
 
 export const statusLinkLimiter = rateLimit({
   windowMs: 1 * 60 * 1000,
   max: 10,
-  message: 'Too many requests, please try again later.'
+  message: 'Too many requests, please try again later.',
+  trustProxy: true,
+  keyGenerator: ipKeyGenerator
 });
